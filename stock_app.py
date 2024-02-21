32b4d3512da1c34db7e62f78c92578fd8b54265c
zgedge1 <zachgedge@gmail.com>
2024-02-21 18:36:05 -0500

    editing syntax

--- stock_app.py
+++ stock_app.py
@@ -1,37 +1,32 @@
 
 import requests
 
+apiKey = "b31fcc2718da460a83388bca924328c7"
+
+
+print("Welcome to the stock app!")
 
 while True:
-    print()
-    symbol = input("Enter a ticker symbol or type exit to quit ")
-    if symbol.lower() == "exit":
-        exit(0)
-
-    api_key = #Enter your API Key
-
-
-    url = f"https://api.twelvedata.com/time_series?symbol={symbol},EUR/USD,ETH/BTC:Huobi,TRP:TSX&interval=1min&apikey={api_key}"
-
+    symbol = input("\nEnter a ticker to search or type 'quit' at anytime to exit:")
+    if symbol.lower() == "quit":
+        exit("\nThank you for using stock app....Exiting Application)")
     
-    stock = requests.get(url).json()
+    URL = f"https://api.twelvedata.com/time_series?symbol={symbol},EUR/USD,ETH/BTC:Huobi,TRP:TSX&interval=1min&apikey={apiKey}"
 
-    date_time = stock[symbol]['values'][0]['datetime']
-    opening_price = stock[symbol]['values'][0]['open']
-    high_price = stock[symbol]['values'][0]['high']
-    low_price = stock[symbol]['values'][0]['low']
-    closing_price = stock[symbol]['values'][0]['close']
+    try:
+        fetchInfo = requests.get(URL).json()
+
+        open_price = fetchInfo[symbol]["values"][0]["open"]
+        high_price = fetchInfo[symbol]["values"][0]["high"]
+        low_price = fetchInfo[symbol]["values"][0]["low"]
+        close_price = fetchInfo[symbol]["values"][0]["close"]
+
+        print(f"\nThe opening price for {symbol} is {open_price}")
+        print(f"The high price for {symbol} is {high_price}")
+        print(f"The low price for {symbol} is {low_price}")
+        print(f"The closing price for {symbol} is {close_price}\n")
+    except:
+        print(f"\nError {symbol} not found.. please enter another ticker.....")
 
 
-    print()
-    print(f"All stock prices are current as of {date_time} ")
-    print()
-    print(f"The opening price for {symbol} is {opening_price}")
-    print()
-    print(f"The high price for {symbol} is {high_price}" )
-    print()
-    print(f"The low price for {symbol} is {low_price}")
-    print()
-    print(f"The closing price for {symbol} is {closing_price}")
-    print()
-    print("*************************************************************")
+
